e3f072875633d73b8625b07155e773b82e7e1214
Gökay Şatır <gokay.satir@collabora.com>
2021-05-11 14:12:30 +0300

    Improve global variable declaration.
    Signed-off-by: Gökay Şatır <gokay.satir@collabora.com>
    Change-Id: I2b926faf8405a6c689ad7d8659707583d93883e9

--- loleaflet/src/layer/tile/CanvasTileLayer.js
+++ loleaflet/src/layer/tile/CanvasTileLayer.js
@@ -779,7 +779,7 @@ L.CanvasTileLayer = L.Layer.extend({
 		this._tileWidthPx = options.tileSize;
 		this._tileHeightPx = options.tileSize;
 
-		// detecting retina displays, adjusting tileWidthPx, tileHeightPx and zoom levels
+		// Detecting retina displays, adjusting zoom levels
 		if (options.detectRetina && L.Browser.retina && options.maxZoom > 0) {
 			options.minZoom = Math.max(0, options.minZoom);
 			options.maxZoom--;
@@ -5580,7 +5580,7 @@ L.CanvasTileLayer = L.Layer.extend({
 			return;
 		}
 
-		if (app.file && app.file.fileBasedView) {
+		if (app.file.fileBasedView) {
 			this._updateFileBasedView();
 			return;
 		}
